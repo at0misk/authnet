befd4ead619b7415ceef8d1da9ecfccdc225d2a2
at0misk <ktp925@gmail.com>
2017-07-14 13:42:07 -0700

    auth

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -179,6 +179,37 @@ end
     return response
   end
 
+def create_customer_profile_from_a_transaction()
+    config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
+
+    transaction = Transaction.new('8e9bG3YDG843', '7aV3ru72Wp9rn5Xc', :gateway => :sandbox)
+
+    
+    request = CreateCustomerProfileFromTransactionRequest.new
+    request.transId = params['id']
+  
+  #You can either specify the customer information in form of customerProfileBaseType object
+  request.customer = CustomerProfileBaseType.new
+  request.customer.merchantCustomerId = "1231232"
+    request.customer.description = "This is a sample customer profile"
+    request.customer.email = "johnsnow@castleblack.com"
+  #  OR   
+  # You can just provide the customer Profile ID
+  # customerProfileId = "123343" 
+  
+    response = transaction.create_customer_profile_from_transaction(request)
+
+
+    if response.messages.resultCode == MessageTypeEnum::Ok
+      puts "Successfully created a customer profile from the transaction id #{response.customerProfileId}"
+    else
+      puts response.messages.messages[0].text
+      raise "Failed to create a customer profile from an existing transaction."
+    end
+    return response
+  end
+
+
 
   def create_subscription_from_customer_profile(profileId = "123213", paymentProfileId = "123213", addressId = "123213")
     # config = YAML.load_file(File.dirname(__FILE__) + "/../credentials.yml")
